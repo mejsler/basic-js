04b8e28d44f9dfc2a6c9b2dbc97e201fe8a30281
mejsler <mejsler@pm.me>
2023-04-15 04:40:04 +0500

    Solution: Mac address

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+const isMAC48Address = str => str.split('-').every(e => e.match(/^[0-9A-F]{2}$/g));
 module.exports = {
   isMAC48Address
 };
--- test/mac-address.test.js
+++ test/mac-address.test.js
@@ -10,7 +10,7 @@ describe('MAC address', () => {
   it.optional('should check if a given string corresponds to MAC-48 address or not', () => {
     assert.strictEqual(isMAC48Address('00-1B-63-84-45-E6'), true);
     assert.strictEqual(isMAC48Address('Z1-1B-63-84-45-E6'), false);
-    assert.strictEqual(isMAC48Address('not a MAC-48 address'), false);
+    assert.strictEqual(isMAC48Address('isMAC48Address'), false);
     assert.strictEqual(isMAC48Address('FF-FF-FF-FF-FF-FF'), true);
     assert.strictEqual(isMAC48Address('G0-00-00-00-00-00'), false);
   });
