2c907eb22304633d5227475295345e806ce5bf00
mejsler <mejsler@pm.me>
2023-04-16 20:48:18 +0500

    *uck extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, objOptions) {
+  let add = '';
+  let addSep = '|';
+  let sep = '+';
+  let keksep = 0;
+  let rep = 1;
+  let repAdd = 1;
+  const objArr = Object.entries(objOptions);
+  for (let [key, value] of objArr) {
+  key === 'addition' 
+  ? add = value
+  : key === 'separator'
+  ? sep = value
+  : key === 'additionSeparator'
+  ? addSep = value
+  : key === 'repeatTimes'
+  ? rep = value
+  : key === 'additionRepeatTimes'
+  ? repAdd = value
+  : 0
+} 
+  for (let key of objArr) {
+  key === 'additionSeparator'
+  ? keksep++
+  : 0
+} 
+  return (str + (add + keksep != 0 ? addSep : '').repeat(repAdd) + sep).repeat(rep - 1) + (str + (add + (repAdd > 1 ? addSep : '')).repeat(repAdd - 1) + add)
 }
+
 
 module.exports = {
   repeater
